b22ec93cb8d89aa3bd6eb8301147cb63e189edda
mbark16 <gambarker16@gmail.com>
2021-06-23 18:34:07 -0600

    No change

--- mauna_loa.R
+++ mauna_loa.R
@@ -1,3 +1,7 @@
 # Matt Barker
 # Module 13 - Mauna Loa
 
+co2 = read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
